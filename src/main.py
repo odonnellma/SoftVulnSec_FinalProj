09a9e70fce2cf221b1537cd1d1b1cf4caaa16278
aisaenic <emilygao713@gmail.com>
2021-04-26 15:19:03 -0700

    adding start and end markers

--- src/main.py
+++ src/main.py
@@ -2,18 +2,23 @@ import subprocess, sys, click
 
 @click.command()
 @click.option("-pcap", "--pcap-input", type=str, help="Path to input pcap")
-#@click.option("-a", "--all-attacks", is_flag=True, help="Enable checking pcap for all attacks")
 @click.option("-f", "--flood", is_flag=True, help="Enable flood checking")
-@click.option("-arp", "--arp", is_flag=True, help="Enable arp cache poisoning checking")
-@click.option("-tcp", "--tcp-rst", is_flag=True, help="Enable tcp rst injection attack checking")
+@click.option("-a", "--arp", is_flag=True, help="Enable arp cache poisoning checking")
+@click.option("-t", "--tcp-rst", is_flag=True, help="Enable tcp rst injection attack checking")
 def run_everything(pcap_input: str, flood: bool, arp: bool, tcp_rst: bool) -> None:
 
     if flood:
+        print('Starting DoS detection\n')
         subprocess.run(["python3", "dos.py", pcap_input])
+        print('Finished DoS detection\n')
     if arp:
+        print('Starting ARP cache poisoning detection\n')
         subprocess.run(["python3", "arp_cache_poisoning.py", pcap_input])
+        print('Finished ARP cache poisoning detection\n')
     if tcp_rst:
+        print('Starting TCP RST injection detection\n')
         subprocess.run(["python3", "tcp_rst_injection.py", pcap_input])
+        print('Finished TCP RST injection detection\n')
 
 if __name__ == "__main__":
     run_everything()
